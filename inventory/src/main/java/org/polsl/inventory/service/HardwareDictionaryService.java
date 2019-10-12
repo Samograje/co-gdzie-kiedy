d9d988fea11f2d18f537bc9322675d7a290c496f
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2019-10-12 20:16:45 +0200

    Dodanie brakującej adnotacji @Autowired

--- inventory/src/main/java/org/polsl/inventory/service/HardwareDictionaryService.java
+++ inventory/src/main/java/org/polsl/inventory/service/HardwareDictionaryService.java
@@ -1,12 +1,14 @@
 package org.polsl.inventory.service;
 
 import org.polsl.inventory.repository.HardwareRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HardwareDictionaryService {
     private HardwareRepository hardwareRepository;
 
+    @Autowired
     public HardwareDictionaryService(HardwareRepository hardwareRepository) {
         this.hardwareRepository = hardwareRepository;
     }
