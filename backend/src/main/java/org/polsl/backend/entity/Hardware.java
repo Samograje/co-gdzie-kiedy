9353e5097cad29601a6a62026d41a3e8b6e83a1e
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2019-10-16 20:27:52 +0200

    Refaktoryzacja

--- backend/src/main/java/org/polsl/backend/entity/Hardware.java
+++ backend/src/main/java/org/polsl/backend/entity/Hardware.java
@@ -1,84 +1,94 @@
 package org.polsl.backend.entity;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import java.util.Set;
 
 /**
- *
+ * Klasa reprezentująca bazodanową encję hardware'u.
  */
 @Entity
 @Table(name = "hardware")
 public class Hardware {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "id")
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @Column(name = "id")
+  private Long id;
 
-    private String name;
+  private String name;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "hardware_dictionary_id", referencedColumnName = "id")
-    private HardwareDictionary hardwareDictionary;
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "hardware_dictionary_id", referencedColumnName = "id")
+  private HardwareDictionary hardwareDictionary;
 
-    @ManyToMany(mappedBy = "hardware")
-    private Set<Affiliation> affiliations;
+  @ManyToMany(mappedBy = "hardware")
+  private Set<Affiliation> affiliations;
 
-    @ManyToMany(mappedBy = "hardware")
-    private Set<ComputerSet> computerSets;
+  @ManyToMany(mappedBy = "hardware")
+  private Set<ComputerSet> computerSets;
 
-    @OneToMany
-    @JoinColumn(name="software")
-    private Set<Software> software;
+  @OneToMany
+  @JoinColumn(name = "software")
+  private Set<Software> software;
 
 
-    public Hardware() {
-    }
+  public Hardware() {
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public HardwareDictionary getHardwareDictionary() {
-        return hardwareDictionary;
-    }
+  public HardwareDictionary getHardwareDictionary() {
+    return hardwareDictionary;
+  }
 
-    public void setHardwareDictionary(HardwareDictionary hardwareDictionary) {
-        this.hardwareDictionary = hardwareDictionary;
-    }
+  public void setHardwareDictionary(HardwareDictionary hardwareDictionary) {
+    this.hardwareDictionary = hardwareDictionary;
+  }
 
-    public Set<Affiliation> getAffiliations() {
-        return affiliations;
-    }
+  public Set<Affiliation> getAffiliations() {
+    return affiliations;
+  }
 
-    public void setAffiliations(Set<Affiliation> affiliations) {
-        this.affiliations = affiliations;
-    }
+  public void setAffiliations(Set<Affiliation> affiliations) {
+    this.affiliations = affiliations;
+  }
 
-    public Set<ComputerSet> getComputerSets() {
-        return computerSets;
-    }
+  public Set<ComputerSet> getComputerSets() {
+    return computerSets;
+  }
 
-    public void setComputerSets(Set<ComputerSet> computerSets) {
-        this.computerSets = computerSets;
-    }
+  public void setComputerSets(Set<ComputerSet> computerSets) {
+    this.computerSets = computerSets;
+  }
 
-    public Set<Software> getSoftware() {
-        return software;
-    }
+  public Set<Software> getSoftware() {
+    return software;
+  }
 
-    public void setSoftware(Set<Software> software) {
-        this.software = software;
-    }
+  public void setSoftware(Set<Software> software) {
+    this.software = software;
+  }
 }
--- backend/src/main/java/org/polsl/backend/entity/Software.java
+++ backend/src/main/java/org/polsl/backend/entity/Software.java
@@ -1,47 +1,53 @@
 package org.polsl.backend.entity;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 /**
- *
+ * Klasa reprezentująca bazodanową encję oprogramowania.
  */
 @Entity
 @Table(name = "software")
 public class Software {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Long id;
 
-    private String name;
+  private String name;
 
-    @ManyToOne
-    @JoinColumn(name="hardware")
-    private Hardware hardware;
+  @ManyToOne
+  @JoinColumn(name = "hardware")
+  private Hardware hardware;
 
-    public Software() {
-    }
+  public Software() {
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public Hardware getHardware() {
-        return hardware;
-    }
+  public Hardware getHardware() {
+    return hardware;
+  }
 
-    public void setHardware(Hardware hardware) {
-        this.hardware = hardware;
-    }
+  public void setHardware(Hardware hardware) {
+    this.hardware = hardware;
+  }
 }
