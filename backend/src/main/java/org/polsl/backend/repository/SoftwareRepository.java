cd3baf5d985b5a958e6b25e15df96e246e4fbbd6
Bartłomiej Szlachta <bartlomiej.szlachta@softiq.pl>
2019-10-26 14:51:04 +0200

    Usunięcie zakomentowanego kodu

--- backend/src/main/java/org/polsl/backend/repository/SoftwareRepository.java
+++ backend/src/main/java/org/polsl/backend/repository/SoftwareRepository.java
@@ -7,6 +7,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SoftwareRepository extends CrudRepository<Software, Long> {
-
-//    List<Software> findAllBySoftwareIsNull();
 }
