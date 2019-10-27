26b674e85ce952ed6bdcf9a25fce086fae6a14b3
Kamil Rusin <lotrusek@gmail.com>
2019-10-27 15:20:16 +0100

    Działający test.

--- backend/src/test/java/org/polsl/backend/DeviceControllerIntegrationTest.java
+++ backend/src/test/java/org/polsl/backend/DeviceControllerIntegrationTest.java
@@ -12,7 +12,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -21,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestPropertySource(locations = "classpath:application-test.properties")
 @Transactional
 @SqlGroup({
-    @Sql(scripts = "/scripts/create-test-device.sql")//TODO:nowy skrypt
+    @Sql(scripts = "/scripts/create-test-device.sql")
 })
 public class DeviceControllerIntegrationTest {
 
@@ -31,7 +33,28 @@ public class DeviceControllerIntegrationTest {
   @Test
   public void givenCorrectRequest_whenGettingDevicesList_thenReturnStatus200AndData() throws Exception{
     mvc.perform(get("/api/devices"))
-        .andExpect(status().is(200));
-
+        .andExpect(status().is(200))
+        .andExpect(jsonPath("$.totalElements").value(5))
+        .andExpect(jsonPath("$.items",hasSize(5)))
+        .andExpect(jsonPath("$.items[0].id").value(1))
+        .andExpect(jsonPath("$.items[0].name").value("HP ProBook"))
+        .andExpect(jsonPath("$.items[0].type").value("Zestaw"))
+        .andExpect(jsonPath("$.items[0].computerSet").value(true))
+        .andExpect(jsonPath("$.items[1].id").value(2))
+        .andExpect(jsonPath("$.items[1].name").value("ACER Laptop"))
+        .andExpect(jsonPath("$.items[1].type").value("Zestaw"))
+        .andExpect(jsonPath("$.items[1].computerSet").value(true))
+        .andExpect(jsonPath("$.items[2].id").value(2))
+        .andExpect(jsonPath("$.items[2].name").value("TP-Link"))
+        .andExpect(jsonPath("$.items[2].type").value("Karta sieciowa"))
+        .andExpect(jsonPath("$.items[2].computerSet").value(false))
+        .andExpect(jsonPath("$.items[3].id").value(4))
+        .andExpect(jsonPath("$.items[3].name").value("i3-5400"))
+        .andExpect(jsonPath("$.items[3].type").value("Procesor"))
+        .andExpect(jsonPath("$.items[3].computerSet").value(false))
+        .andExpect(jsonPath("$.items[4].id").value(5))
+        .andExpect(jsonPath("$.items[4].name").value("HD 7770"))
+        .andExpect(jsonPath("$.items[4].type").value("Karta graficzna"))
+        .andExpect(jsonPath("$.items[4].computerSet").value(false));
   }
 }
