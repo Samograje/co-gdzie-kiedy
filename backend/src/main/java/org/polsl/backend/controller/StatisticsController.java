a060e50170d0e4c2caec510bd20c81cf5d20a5db
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2019-10-28 07:35:46 +0100

    Refaktoryzacja kodu

--- backend/src/main/java/org/polsl/backend/controller/StatisticsController.java
+++ backend/src/main/java/org/polsl/backend/controller/StatisticsController.java
@@ -1,11 +1,5 @@
 package org.polsl.backend.controller;
 
-import org.polsl.backend.entity.Affiliation;
-import org.polsl.backend.entity.Software;
-import org.polsl.backend.repository.AffiliationRepository;
-import org.polsl.backend.repository.ComputerSetRepository;
-import org.polsl.backend.repository.HardwareRepository;
-import org.polsl.backend.repository.SoftwareRepository;
 import org.polsl.backend.service.StatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Kontroler odpowiedzialny za zarządzanie statystykami na stronie głównej.
  */
 @RestController
-@RequestMapping("api/statistics") //TODO: ustalić endpoint strony głównej
+@RequestMapping("api/statistics")
 public class StatisticsController {
 
   private final StatisticsService statisticsService;
@@ -27,6 +21,11 @@ public class StatisticsController {
     this.statisticsService = statisticsService;
   }
 
+  /**
+   * Endpoint obsługujący uzyskiwanie statystyk z bazy danych.
+   *
+   * @return statystyki z bazy danych
+   */
   @GetMapping
   public ResponseEntity<?> getAllStatistics() {
     return ResponseEntity.ok(statisticsService.getAllStatistics());
--- backend/src/main/java/org/polsl/backend/service/StatisticsService.java
+++ backend/src/main/java/org/polsl/backend/service/StatisticsService.java
@@ -1,6 +1,5 @@
 package org.polsl.backend.service;
 
-import org.polsl.backend.dto.PaginatedResult;
 import org.polsl.backend.dto.statistics.StatisticsOutputDTO;
 import org.polsl.backend.repository.AffiliationRepository;
 import org.polsl.backend.repository.ComputerSetRepository;
@@ -9,6 +8,9 @@ import org.polsl.backend.repository.SoftwareRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * Logika biznesowa statystyk dotyczących bazy danych.
+ */
 @Service
 public class StatisticsService {
   private final AffiliationRepository affiliationRepository;
@@ -23,10 +25,6 @@ public class StatisticsService {
     this.hardwareRepository = hardwareRepository;
     this.computerSetRepository = computerSetRepository;
     this.softwareRepository = softwareRepository;
-  }
-
-  public long count() {
-    return affiliationRepository.count();
   }
 
   public StatisticsOutputDTO getAllStatistics() {
