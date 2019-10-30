14864f980a209d5af171edb368dc0127856ad5de
Kamil Błoński <kamil.blonski123@gmail.com>
2019-10-30 18:15:27 +0100

    Implementacja COmputerSetSoftwareKey.

--- backend/src/main/java/org/polsl/backend/key/ComputerSetSoftwareKey.java
+++ backend/src/main/java/org/polsl/backend/key/ComputerSetSoftwareKey.java
@@ -1,4 +1,52 @@
 package org.polsl.backend.key;
 
-public class ComputerSetSoftwareKey {
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Embeddable
+public class ComputerSetSoftwareKey implements Serializable {
+    @Column(name = "software_id")
+    Long softwareId;
+    @Column(name = "computer_sets_id")
+    Long computerSetId;
+    @Column(name = "valid_from")
+    LocalDateTime validFrom;
+
+    public ComputerSetSoftwareKey() { }
+
+    //region gettersAndSetters
+    public Long getSoftwareId(){ return softwareId; }
+
+    public void setSoftwareId(Long softwareId) { this.softwareId = softwareId; }
+
+    public Long getComputerSetId() { return computerSetId; }
+
+    public void setComputerSetId(Long computerSetId) { this.computerSetId = computerSetId; }
+
+    public LocalDateTime getValidFrom() { return validFrom; }
+
+    public void setValidFrom(LocalDateTime validFrom) { this.validFrom = validFrom; }
+
+    //endregion
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o){
+            return true;
+        }
+        if(o == null || getClass() != o.getClass()){
+            return false;
+        }
+        ComputerSetSoftwareKey computerSetSoftwareKey = (ComputerSetSoftwareKey)o;
+        return Objects.equals(softwareId, computerSetSoftwareKey.softwareId)
+                && Objects.equals(computerSetId, computerSetSoftwareKey.computerSetId)
+                && Objects.equals(validFrom, computerSetSoftwareKey.validFrom);
+    }
+
+    @Override
+    public int hashCode(){ return Objects.hash(softwareId, computerSetId, validFrom); }
+
 }
