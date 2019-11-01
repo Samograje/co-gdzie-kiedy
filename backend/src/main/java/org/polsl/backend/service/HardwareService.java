da0fe6c0113e12102228a6c44c851fa6da6e2d1f
Kamil Rusin <lotrusek@gmail.com>
2019-11-01 17:35:42 +0100

    Poprawki do dodawania klas pośrednich. Użytko klas Key.

--- backend/src/main/java/org/polsl/backend/service/HardwareService.java
+++ backend/src/main/java/org/polsl/backend/service/HardwareService.java
@@ -3,15 +3,20 @@ package org.polsl.backend.service;
 import org.polsl.backend.dto.PaginatedResult;
 import org.polsl.backend.dto.hardware.HardwareInputDTO;
 import org.polsl.backend.dto.hardware.HardwareOutputDTO;
-import org.polsl.backend.entity.*;
+import org.polsl.backend.entity.AffiliationHardware;
+import org.polsl.backend.entity.ComputerSetHardware;
+import org.polsl.backend.entity.Hardware;
+import org.polsl.backend.entity.HardwareDictionary;
 import org.polsl.backend.exception.NotFoundException;
+import org.polsl.backend.key.AffiliationHardwareKey;
+import org.polsl.backend.key.ComputerSetHardwareKey;
 import org.polsl.backend.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -22,14 +27,18 @@ public class HardwareService {
   private final HardwareRepository hardwareRepository;
   private final HardwareDictionaryRepository hardwareDictionaryRepository;
   private final ComputerSetRepository computerSetRepository;
+  private final ComputerSetHardwareRepository computerSetHardwareRepository;
   private final AffiliationRepository affiliationRepository;
+  private final AffiliationHardwareRepository affiliationHardwareRepository;
 
   @Autowired
-  public HardwareService(HardwareRepository hardwareRepository, HardwareDictionaryRepository hardwareDictionaryRepository, ComputerSetRepository computerSetRepository, ComputerSetHardwareRepository computerSetHardwareRepository, AffiliationRepository affiliationRepository) {
+  public HardwareService(HardwareRepository hardwareRepository, HardwareDictionaryRepository hardwareDictionaryRepository, ComputerSetRepository computerSetRepository, ComputerSetHardwareRepository computerSetHardwareRepository, ComputerSetHardwareRepository computerSetHardwareRepository1, AffiliationRepository affiliationRepository, AffiliationHardwareRepository affiliationHardwareRepository) {
     this.hardwareRepository = hardwareRepository;
     this.hardwareDictionaryRepository = hardwareDictionaryRepository;
     this.computerSetRepository = computerSetRepository;
+    this.computerSetHardwareRepository = computerSetHardwareRepository1;
     this.affiliationRepository = affiliationRepository;
+    this.affiliationHardwareRepository = affiliationHardwareRepository;
   }
 
   public PaginatedResult<HardwareOutputDTO> getAllSoloHardware() {
@@ -48,42 +57,35 @@ public class HardwareService {
     return response;
   }
 
+  @Transactional
   public void createHardware(HardwareInputDTO request) {
     Hardware hardware = new Hardware();
     hardware.setName(request.getName());
-
     HardwareDictionary hardwareDictionary = hardwareDictionaryRepository.findById(request.getDictionaryId())
         .orElseThrow(() -> new NotFoundException("słownik urządzeń", "id", request.getDictionaryId()));
     hardware.setHardwareDictionary(hardwareDictionary);
+    hardwareRepository.save(hardware);
 
-    if (request.getComputerSetId() != null) {
-      ComputerSet computerSet = computerSetRepository.findById(request.getComputerSetId())
-          .orElseThrow(() -> new NotFoundException("zestawy komputerowe", "id", request.getComputerSetId()));
+    if(request.getComputerSetId() != null){
+      ComputerSetHardwareKey computerSetHardwareKey = new ComputerSetHardwareKey();
+      computerSetHardwareKey.setHardwareId(hardware.getId());
+      computerSetHardwareKey.setComputerSetId(computerSetRepository.findById(request.getComputerSetId())
+          .orElseThrow(() -> new NotFoundException("zestawy komputerowe", "id", request.getComputerSetId())).getId());
+      computerSetHardwareKey.setValidFrom(LocalDateTime.now());
       ComputerSetHardware computerSetHardware = new ComputerSetHardware();
-      computerSetHardware.setComputerSet(computerSet);
-      computerSetHardware.setHardware(hardware);
-      computerSetHardware.setValidFrom(LocalDateTime.now());
+      computerSetHardware.setId(computerSetHardwareKey);
       computerSetHardware.setValidTo(null);
-
-      HashSet<ComputerSetHardware> computerSetHardwareSet = new HashSet<>();
-      computerSetHardwareSet.add(computerSetHardware);
-      hardware.setComputerSetHardwareSet(computerSetHardwareSet);
-    } else {
-      hardware.setComputerSetHardwareSet(null);
+      computerSetHardwareRepository.save(computerSetHardware);
     }
 
-    Affiliation affiliation = affiliationRepository.findByIdAndIsDeletedIsFalse(request.getAffiliationId())
-        .orElseThrow(() -> new NotFoundException("przynależność", "id", request.getAffiliationId()));
+    AffiliationHardwareKey affiliationHardwareKey = new AffiliationHardwareKey();
+    affiliationHardwareKey.setHardwareId(hardware.getId());
+    affiliationHardwareKey.setAffiliationId(affiliationRepository.findByIdAndIsDeletedIsFalse(request.getAffiliationId())
+        .orElseThrow(() -> new NotFoundException("przynależność", "id", request.getAffiliationId())).getId());
+    affiliationHardwareKey.setValidFrom(LocalDateTime.now());
     AffiliationHardware affiliationHardware = new AffiliationHardware();
-    affiliationHardware.setAffiliation(affiliation);
-    affiliationHardware.setValidFrom(LocalDateTime.now());
+    affiliationHardware.setId(affiliationHardwareKey);
     affiliationHardware.setValidTo(null);
-    affiliationHardware.setHardware(hardware);
-
-    HashSet<AffiliationHardware> affiliationHardwareSet = new HashSet<>();
-    affiliationHardwareSet.add(affiliationHardware);
-    hardware.setAffiliationHardwareSet(affiliationHardwareSet);
-
-    hardwareRepository.save(hardware);
+    affiliationHardwareRepository.save(affiliationHardware);
   }
 }
