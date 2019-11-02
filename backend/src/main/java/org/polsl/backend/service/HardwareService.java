9b9c91cc7912dc3b5615f23bef67c0672fb7c1a5
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2019-11-02 23:42:11 +0100

    Dostosowanie metody dodającej hardware do nowej wersji obsługi kluczy

--- backend/src/main/java/org/polsl/backend/service/HardwareService.java
+++ backend/src/main/java/org/polsl/backend/service/HardwareService.java
@@ -3,19 +3,23 @@ package org.polsl.backend.service;
 import org.polsl.backend.dto.PaginatedResult;
 import org.polsl.backend.dto.hardware.HardwareInputDTO;
 import org.polsl.backend.dto.hardware.HardwareOutputDTO;
+import org.polsl.backend.entity.Affiliation;
 import org.polsl.backend.entity.AffiliationHardware;
+import org.polsl.backend.entity.ComputerSet;
 import org.polsl.backend.entity.ComputerSetHardware;
 import org.polsl.backend.entity.Hardware;
 import org.polsl.backend.entity.HardwareDictionary;
 import org.polsl.backend.exception.NotFoundException;
-import org.polsl.backend.key.AffiliationHardwareKey;
-import org.polsl.backend.key.ComputerSetHardwareKey;
-import org.polsl.backend.repository.*;
+import org.polsl.backend.repository.AffiliationHardwareRepository;
+import org.polsl.backend.repository.AffiliationRepository;
+import org.polsl.backend.repository.ComputerSetHardwareRepository;
+import org.polsl.backend.repository.ComputerSetRepository;
+import org.polsl.backend.repository.HardwareDictionaryRepository;
+import org.polsl.backend.repository.HardwareRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,7 +36,12 @@ public class HardwareService {
   private final AffiliationHardwareRepository affiliationHardwareRepository;
 
   @Autowired
-  public HardwareService(HardwareRepository hardwareRepository, HardwareDictionaryRepository hardwareDictionaryRepository, ComputerSetRepository computerSetRepository, ComputerSetHardwareRepository computerSetHardwareRepository, ComputerSetHardwareRepository computerSetHardwareRepository1, AffiliationRepository affiliationRepository, AffiliationHardwareRepository affiliationHardwareRepository) {
+  public HardwareService(HardwareRepository hardwareRepository,
+                         HardwareDictionaryRepository hardwareDictionaryRepository,
+                         ComputerSetRepository computerSetRepository,
+                         ComputerSetHardwareRepository computerSetHardwareRepository1,
+                         AffiliationRepository affiliationRepository,
+                         AffiliationHardwareRepository affiliationHardwareRepository) {
     this.hardwareRepository = hardwareRepository;
     this.hardwareDictionaryRepository = hardwareDictionaryRepository;
     this.computerSetRepository = computerSetRepository;
@@ -66,26 +75,16 @@ public class HardwareService {
     hardware.setHardwareDictionary(hardwareDictionary);
     hardwareRepository.save(hardware);
 
-    if(request.getComputerSetId() != null){
-      ComputerSetHardwareKey computerSetHardwareKey = new ComputerSetHardwareKey();
-      computerSetHardwareKey.setHardwareId(hardware.getId());
-      computerSetHardwareKey.setComputerSetId(computerSetRepository.findById(request.getComputerSetId())
-          .orElseThrow(() -> new NotFoundException("zestawy komputerowe", "id", request.getComputerSetId())).getId());
-      computerSetHardwareKey.setValidFrom(LocalDateTime.now());
-      ComputerSetHardware computerSetHardware = new ComputerSetHardware();
-      computerSetHardware.setId(computerSetHardwareKey);
-      computerSetHardware.setValidTo(null);
+    if (request.getComputerSetId() != null) {
+      ComputerSet computerSet = computerSetRepository.findById(request.getComputerSetId())
+          .orElseThrow(() -> new NotFoundException("zestaw komputerowy", "id", request.getComputerSetId()));
+      ComputerSetHardware computerSetHardware = new ComputerSetHardware(computerSet, hardware);
       computerSetHardwareRepository.save(computerSetHardware);
     }
 
-    AffiliationHardwareKey affiliationHardwareKey = new AffiliationHardwareKey();
-    affiliationHardwareKey.setHardwareId(hardware.getId());
-    affiliationHardwareKey.setAffiliationId(affiliationRepository.findByIdAndIsDeletedIsFalse(request.getAffiliationId())
-        .orElseThrow(() -> new NotFoundException("przynależność", "id", request.getAffiliationId())).getId());
-    affiliationHardwareKey.setValidFrom(LocalDateTime.now());
-    AffiliationHardware affiliationHardware = new AffiliationHardware();
-    affiliationHardware.setId(affiliationHardwareKey);
-    affiliationHardware.setValidTo(null);
+    Affiliation affiliation = affiliationRepository.findByIdAndIsDeletedIsFalse(request.getAffiliationId())
+        .orElseThrow(() -> new NotFoundException("przynależność", "id", request.getAffiliationId()));
+    AffiliationHardware affiliationHardware = new AffiliationHardware(affiliation, hardware);
     affiliationHardwareRepository.save(affiliationHardware);
   }
 }
