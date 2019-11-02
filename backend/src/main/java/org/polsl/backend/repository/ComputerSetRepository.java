141f765e90ff68dd52d50c28a36ae45e9afb34ca
Bartłomiej Szlachta <bartlomiej.szlachta@softiq.pl>
2019-11-02 23:27:41 +0100

    Poprawa błędu z mastera

--- backend/src/main/java/org/polsl/backend/repository/ComputerSetRepository.java
+++ backend/src/main/java/org/polsl/backend/repository/ComputerSetRepository.java
@@ -1,10 +1,9 @@
 package org.polsl.backend.repository;
 
 import org.polsl.backend.entity.ComputerSet;
-import org.polsl.backend.key.AffiliationComputerSetKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ComputerSetRepository extends JpaRepository<ComputerSet, AffiliationComputerSetKey> {
+public interface ComputerSetRepository extends JpaRepository<ComputerSet, Long> {
 }
