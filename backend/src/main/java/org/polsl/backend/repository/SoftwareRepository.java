142b28534980d299d7baae68cc7466dcedcbdb17
Kamil Błoński <kamil.blonski123@gmail.com>
2019-11-03 14:22:55 +0100

    Zmiany w repo.

--- backend/src/main/java/org/polsl/backend/repository/SoftwareRepository.java
+++ backend/src/main/java/org/polsl/backend/repository/SoftwareRepository.java
@@ -2,14 +2,14 @@ package org.polsl.backend.repository;
 
 
 import org.polsl.backend.entity.Software;
-import org.springframework.data.repository.CrudRepository;
+import org.polsl.backend.key.ComputerSetSoftwareKey;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface SoftwareRepository extends CrudRepository<Software, Long> {
-
+public interface SoftwareRepository extends JpaRepository<Software, ComputerSetSoftwareKey> {
     Optional<Software> findAllById(Long id);
     Optional<Software> findById(Long id);
 }
