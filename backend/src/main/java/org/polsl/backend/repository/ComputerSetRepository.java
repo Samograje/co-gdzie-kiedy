1ab7fbb892e8f84b1b5bea2fbd209483bcecbe2c
Kamil Błoński <kamil.blonski123@gmail.com>
2019-11-03 13:23:42 +0100

    Dodanie dwóch metod w dwóch klasach w repo.

--- backend/src/main/java/org/polsl/backend/repository/ComputerSetRepository.java
+++ backend/src/main/java/org/polsl/backend/repository/ComputerSetRepository.java
@@ -5,6 +5,9 @@ import org.polsl.backend.key.AffiliationComputerSetKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ComputerSetRepository extends JpaRepository<ComputerSet, AffiliationComputerSetKey> {
+    Optional<ComputerSet> findById(Long id);
 }
--- backend/src/main/java/org/polsl/backend/repository/ComputerSetSoftwareRepository.java
+++ backend/src/main/java/org/polsl/backend/repository/ComputerSetSoftwareRepository.java
@@ -5,6 +5,9 @@ import org.polsl.backend.key.ComputerSetSoftwareKey;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface ComputerSetSoftwareRepository extends CrudRepository<ComputerSetSoftware, ComputerSetSoftwareKey> {
+    Set<ComputerSetSoftware> findAllBySoftwareId(Long id);
 }
