c64f75b41350da9f49f5f3df44fee2a057111459
Kamil Rusin <48494453+kamil-rusin@users.noreply.github.com>
2019-11-05 11:20:22 +0100

    Dodanie edycji sprzętu (#44)
    * Utworzono funkcję modyfikacji sprzętu. Nietestowana.
    
    * Drobne poprawki
    
    * Wszystkie testy przeszły.
    
    * Poprawiono importy. Poprawiono niepotrzebne tworzenie rekordów przy tym samym affiliationId.
    
    * Pozbycie się warningów nullpointerexception.
    
    * Dodano SQL-e
    
    * Usunięto zbędny kod w HardwareService
    
    * Refaktoryzacja oraz dodanie komentarzy TODO

--- backend/src/main/java/org/polsl/backend/repository/ComputerSetHardwareRepository.java
+++ backend/src/main/java/org/polsl/backend/repository/ComputerSetHardwareRepository.java
@@ -2,9 +2,16 @@ package org.polsl.backend.repository;
 
 import org.polsl.backend.entity.ComputerSetHardware;
 import org.polsl.backend.key.ComputerSetHardwareKey;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ComputerSetHardwareRepository extends CrudRepository<ComputerSetHardware, ComputerSetHardwareKey> {
+
+  @Query(value = "SELECT * FROM public.computer_sets_hardware WHERE hardware_id = :id ORDER BY valid_from desc LIMIT 1;",
+      nativeQuery = true)
+  Optional<ComputerSetHardware> findNewestRowForHardware(Long id);
 }
