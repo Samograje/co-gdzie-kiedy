f947c99f14ae0f6a2243cca1cea80fee3adfedab
Bartłomiej Szlachta <bartlomiej.szlachta@softiq.pl>
2019-11-11 14:43:11 +0100

    Usunięcie metody z innego zadania

--- backend/src/main/java/org/polsl/backend/service/HardwareService.java
+++ backend/src/main/java/org/polsl/backend/service/HardwareService.java
@@ -68,17 +68,6 @@ public class HardwareService {
     return response;
   }
 
-  public HardwareOutputDTO getHardware(Long id){
-    Hardware hardware = hardwareRepository.findById(id)
-        .orElseThrow(() -> new NotFoundException("sprzęt", "id", id));
-    HardwareOutputDTO dto = new HardwareOutputDTO();
-    dto.setName(hardware.getName());
-    dto.setId(hardware.getId());
-    dto.setType(hardware.getHardwareDictionary().getValue());
-    //TODO: AffiliationHardware, ComputerSetHardware
-    return dto;
-  }
-
   @Transactional
   public void createHardware(HardwareInputDTO request) {
     Hardware hardware = new Hardware();
