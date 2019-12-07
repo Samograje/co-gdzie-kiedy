87b0895fac84885ef300b2a273bb46ebbc2ccf78
Kamil Rusin <lotrusek@gmail.com>
2019-12-07 20:11:42 +0100

    Inicjalizacja fetch'a

--- frontend/src/components/hardware/HardwareListContainer.jsx
+++ frontend/src/components/hardware/HardwareListContainer.jsx
@@ -2,9 +2,38 @@ import React, {Component} from 'react';
 import HardwareListComponent from './HardwareListComponent';
 
 class HardwareListContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      error: false
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/hardware')
+        .then((response) => response.json())
+        .then((response) => {
+          this.setState({
+            loading: false,
+            error: false,
+            ...response
+          })
+        })
+        .catch((response) => {
+          this.setState({
+            loading: false,
+            error: true
+          });
+        })
+  }
+
+
   render() {
     return (
-      <HardwareListComponent/>
+      <HardwareListComponent
+          {...this.state}
+      />
     );
   }
 }
