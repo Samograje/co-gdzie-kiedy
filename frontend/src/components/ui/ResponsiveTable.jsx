4a6f30e84daf78af04da9f26568fc29d47aa6ac1
Bartłomiej Szlachta <bartlomiej.szlachta@softiq.pl>
2019-12-10 22:11:06 +0100

    feat: tryb mobilny + poprawki

--- frontend/src/components/ui/ResponsiveTable.jsx
+++ frontend/src/components/ui/ResponsiveTable.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from "react-native";
+import {FlatList, ScrollView, StyleSheet, Text, View} from "react-native";
 
 class ResponsiveTable extends React.Component {
   constructor(props) {
@@ -27,26 +27,39 @@ class ResponsiveTable extends React.Component {
       footerActions,
     } = this.props;
 
-    const smallLayout = (
-      <View>
-        <Text>Small layout</Text>
+    const mobileLayout = (
+      <View style={styles.list}>
+        <FlatList
+          data={items}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => (
+            <View style={styles.item}>
+              {columns.map((column, key) => (
+                <View style={styles.row} key={key}>
+                  <Text style={[styles.label, styles.text]}>{column.label}</Text>
+                  <Text style={[styles.value, styles.text]}>{item[column.name]}</Text>
+                </View>
+              ))}
+            </View>
+          )}
+        />
       </View>
     );
 
-    const largeLayout = (
+    const wideLayout = (
       <View style={styles.table}>
         <View style={styles.tr}>
-          {columns.map((column) => (
-            <View style={styles.th}>
-              <Text style={styles.thText}>{column.label}</Text>
+          {columns.map((column, key) => (
+            <View style={styles.th} key={key}>
+              <Text style={[styles.thText, styles.text]}>{column.label}</Text>
             </View>
           ))}
         </View>
         {items.map((item, rowId) => (
-          <View style={[styles.tr, rowId % 2 !== 0 && styles.greyRow]}>
-            {columns.map((column) => (
-              <View style={styles.td}>
-                <Text style={styles.tdText}>{item[column.name]}</Text>
+          <View style={[styles.tr, rowId % 2 !== 0 && styles.greyRow]} key={rowId}>
+            {columns.map((column, key) => (
+              <View style={styles.td} key={key}>
+                <Text style={[styles.tdText, styles.text]}>{item[column.name]}</Text>
               </View>
             ))}
           </View>
@@ -55,33 +68,59 @@ class ResponsiveTable extends React.Component {
     );
 
     return (
-      <View style={styles.container} onLayout={this.handleLayout}>
-        {this.state.isWide ? largeLayout : smallLayout}
-      </View>
+      <ScrollView>
+        <View style={styles.container} onLayout={this.handleLayout}>
+          {this.state.isWide ? wideLayout : mobileLayout}
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {},
+
+  // dotyczy widoku mobilnego
+  list: {},
+  item: {},
+  row: {
+    flexDirection: 'row',
+  },
+  label: {
+    flex: 1,
+    textAlign: 'right',
+    marginRight: 5,
+    fontWeight: 'bold',
+  },
+  value: {
+    marginLeft: 5,
+    flex: 1,
+  },
+
+  // dotyczy widoku tabeli na szerokie ekrany
   table: {},
   tr: {
     flexDirection: 'row',
-  },
-  greyRow: {
-    backgroundColor: 'lightgrey',
   },
   th: {
     flex: 1,
     backgroundColor: 'darkgrey',
   },
   thText: {
-    fontSize: 16,
+    fontWeight: 'bold',
   },
   td: {
     flex: 1,
   },
   tdText: {},
+
+  // dotyczy obu widoków
+  text: {
+    fontSize: 16,
+  },
+  greyRow: {
+    backgroundColor: 'lightgrey',
+  },
 });
 
 export default ResponsiveTable;
