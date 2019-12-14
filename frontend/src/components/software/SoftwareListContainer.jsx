dd8efb5fea39b547298e44c537c1ea6730ecedb0
Kamil Błoński <kamil.blonski123@gmail.com>
2019-12-14 23:06:15 +0100

    Wyświetla się w tabeli tylko nazwa, szukam błędu.

--- frontend/src/components/software/SoftwareListContainer.jsx
+++ frontend/src/components/software/SoftwareListContainer.jsx
@@ -2,9 +2,70 @@ import React, {Component} from 'react';
 import SoftwareListComponent from './SoftwareListComponent';
 
 class SoftwareListContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      error: false,
+      items: [],
+      totalElements: null,
+    };
+  }
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = () => {
+    fetch('/api/software')
+        .then((response) => response.json())
+        .then((response) => {
+          this.setState({
+            loading: false,
+            ...response,
+          });
+        })
+        .catch(() => {
+          this.setState({
+            loading: false,
+            error: true,
+          });
+        })
+  };
+
   render() {
+    const columns = [
+      {
+        name: 'name',
+        label: 'Nazwa',
+      },
+      {
+        inventoryNumber: 'inventoryNumber',
+        label: 'Numer inwentażowy',
+      },
+      {
+        key: 'key',
+        label: 'Klucz produktu',
+      },
+      {
+        availableKeys: 'availableKeys',
+        label: 'Ilość dostępnych kluczy',
+      },
+      {
+        duration: 'duration',
+        label: 'Ważna do',
+      },
+      {
+        validTo: 'validTo',
+        label: 'Usunięta dnia',
+      },
+    ];
+
     return (
-      <SoftwareListComponent/>
+      <SoftwareListComponent
+          onFetchData={this.fetchData}
+          columns={columns}
+          {...this.state}
+      />
     );
   }
 }
