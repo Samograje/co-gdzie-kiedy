b401e10626045eeef2e288495f07a991561f0d28
Bartłomiej Szlachta <bartlomiej.szlachta@softiq.pl>
2019-12-22 13:06:06 +0100

    fix: add todo

--- frontend/src/APIClient.js
+++ frontend/src/APIClient.js
@@ -3,6 +3,8 @@ import {Platform} from 'react-native';
 const baseUrl = 'https://dry-wildwood-77221.herokuapp.com';
 
 const request = (url, options) => {
+  // TODO: docelowo trzeba to rozwiązać w inny sposób - bez proxy w package.json
+
   let finalUrl;
 
   if (Platform.OS === 'web') {
