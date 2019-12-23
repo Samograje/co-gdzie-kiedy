100f4fa2a5193eee36b563e8bef6c835de1f882d
Kamil Rusin <lotrusek@gmail.com>
2019-12-23 16:59:15 +0100

    Dodano przyciski w formularzu Hardware'u.

--- frontend/src/components/hardware/HardwareDetailsComponent.jsx
+++ frontend/src/components/hardware/HardwareDetailsComponent.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {Button, StyleSheet, Text} from 'react-native';
 
 const HardwareDetailsComponent = (props) => {
-  return (
-    <Text>Tutaj znajdzie się formularz hardware'u</Text>
-  );
+    return (
+        <>
+            <Text>Tutaj znajdzie się formularz hardware'u</Text>
+            <Button
+                title="Zapisz"
+                onPress={props.onSubmit}
+            />
+            <Button
+                title="Wróć"
+                onPress={props.onReject}
+            />
+        </>
+    );
 };
 
 const styles = StyleSheet.create({
-
+    container: {
+        justifyContent: 'center',
+    },
 });
 
 export default HardwareDetailsComponent;
--- frontend/src/components/hardware/HardwareDetailsContainer.jsx
+++ frontend/src/components/hardware/HardwareDetailsContainer.jsx
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 import HardwareDetailsComponent from './HardwareDetailsComponent';
+import AffiliationDetailsComponent from "../affiliations/AffiliationDetailsComponent";
 
 class HardwareDetailsContainer extends Component {
-  render() {
-    return (
-      <HardwareDetailsComponent/>
-    );
-  }
+
+    onSubmit = () => this.props.history.goBack();
+
+    onReject = () => this.props.history.goBack();
+
+    render() {
+        return (
+            <HardwareDetailsComponent
+                onSubmit={this.onSubmit}
+                onReject={this.onReject}
+            />
+        );
+    }
 }
 
 export default HardwareDetailsContainer;
