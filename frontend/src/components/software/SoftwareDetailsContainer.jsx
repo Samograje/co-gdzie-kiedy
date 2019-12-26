c0f3515dfa806cf70799038fb43c6f4104e4800e
Kamil Błoński <kamil.blonski123@gmail.com>
2019-12-26 16:59:17 +0100

    Przycisk zapisz/usuń powodują powrót do listy oprogramowania.

--- frontend/src/components/software/SoftwareDetailsContainer.jsx
+++ frontend/src/components/software/SoftwareDetailsContainer.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 import SoftwareDetailsComponent from './SoftwareDetailsComponent';
+import AffiliationDetailsComponent from "../affiliations/AffiliationDetailsComponent";
 
 class SoftwareDetailsContainer extends Component {
+  onSubmit = () => this.props.history.goBack();
+
+  onReject = () => this.props.history.goBack();
   render() {
     return (
-      <SoftwareDetailsComponent/>
+      <SoftwareDetailsComponent
+          onSubmit={this.onSubmit}
+          onReject={this.onReject}
+      />
     );
   }
 }
