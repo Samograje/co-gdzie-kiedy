44dd24a171d26499a711c6d4d42d60ba17fd7831
Kamil Błoński <kamil.blonski123@gmail.com>
2019-12-26 17:59:38 +0100

    Utworzenie podstawowej wersji formularza.

--- frontend/src/components/software/SoftwareDetailsComponent.jsx
+++ frontend/src/components/software/SoftwareDetailsComponent.jsx
@@ -1,24 +1,68 @@
 import React from 'react';
-import {Button, StyleSheet, Text} from 'react-native';
+import {Button,
+        StyleSheet,
+        Text,
+        View,
+        TextInput,
+        } from 'react-native';
 
 const SoftwareDetailsComponent = (props) => {
   return (
-    <>
-      <Text>Tutaj znajdzie się formularz software'u</Text>
-      <Button
+    <View style={styles.addform}>
+      <Text style={styles.header}>Formularz dodawania lub edycji oprogramowania</Text>
+        <View style={styles.onelineelement}>
+            <Text style={styles.labeltext}>Nazwa oprogramowania:</Text>
+            <TextInput style={styles.textinput} placeholder={"np. Mathematica"}/>
+        </View>
+        <View style={styles.onelineelement}>
+            <Text style={styles.labeltext}>Klucz produktu:</Text>
+            <TextInput style={styles.textinput} placeholder={"np. T847-54GF-7845-FSF5"}/>
+        </View>
+        <View style={styles.onelineelement}>
+            <Text style={styles.labeltext}>Ilość dostępnych kluczy:</Text>
+            <TextInput style={styles.textinput} placeholder={"np. 5"}/>
+        </View>
+        <View style={styles.onelineelement}>
+            <Text style={styles.labeltext}>Ważna do:</Text>
+            <TextInput style={styles.textinput} placeholder={"Data końca licencji. "}/>
+        </View>
+
+        <Button
           title="Zapisz"
           onPress={props.onSubmit}
-      />
-      <Button
+        />
+        <Button
           title="Wróć"
           onPress={props.onReject}
-      />
-    </>
+        />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+    addform: {
+        backgroundColor: '#f4f4f4',
+        alignSelf: 'center',
+        padding: 15,
+    },
+    labeltext: {
+      marginRight: 10,
+    },
+    header: {
+        fontSize: 24,
+        paddingBottom: 10,
+        marginBottom: 40,
+        borderBottomColor: '#199187',
+        borderBottomWidth: 1,
+    },
+    textinput: {
 
+    },
+    onelineelement: {
+        flexDirection: 'row',
+        // backgroundColor: 'yellow',
+        marginBottom: 15,
+    }
 });
 
 export default SoftwareDetailsComponent;
--- frontend/src/components/software/SoftwareDetailsContainer.jsx
+++ frontend/src/components/software/SoftwareDetailsContainer.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import SoftwareDetailsComponent from './SoftwareDetailsComponent';
-import AffiliationDetailsComponent from "../affiliations/AffiliationDetailsComponent";
 
 class SoftwareDetailsContainer extends Component {
   onSubmit = () => this.props.history.goBack();
@@ -8,10 +7,10 @@ class SoftwareDetailsContainer extends Component {
   onReject = () => this.props.history.goBack();
   render() {
     return (
-      <SoftwareDetailsComponent
-          onSubmit={this.onSubmit}
-          onReject={this.onReject}
-      />
+          <SoftwareDetailsComponent
+              onSubmit={this.onSubmit}
+              onReject={this.onReject}
+          />
     );
   }
 }
