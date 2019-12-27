b41dfbebc8fbda5da710164cc0ce63992af94e29
Kamil Błoński <kamil.blonski123@gmail.com>
2019-12-27 18:00:03 +0100

    Drobne zmiany w kodzie.

--- frontend/src/components/software/SoftwareDetailsContainer.jsx
+++ frontend/src/components/software/SoftwareDetailsContainer.jsx
@@ -4,70 +4,53 @@ import SoftwareDetailsComponent from './SoftwareDetailsComponent';
 class SoftwareDetailsContainer extends Component {
   constructor(props) {
     super(props);
-    this.registerCall = this.registerCall.bind(this);
+    this.addCall = this.addCall.bind(this);
     this.state = {
       name: '',
       key: '',
       availableKeys: '',
       duration: '',
-      baseUrl: 'http://localhost:8080/api/software'
     };
   }
-  registerCall(){
-  console.log(this.state.name);
-    var that = this;
-    var url = that.state.baseUrl;
-    fetch(url,{
+  addCall(){
+    fetch('http://localhost:8080/api/software',{
       method: 'POST',
       mode: 'cors',
-      body: JSON.stringify({
-        "name": this.state.name,
-        "key": this.state.key,
-        "availableKeys": this.state.availableKeys,
-        "duration": this.state.duration
-      }),
+      body: JSON.stringify(this.state),
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
-        'Cache-Control': 'no-cache',
-        'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
       }
-    }).then(function (response) {
-      return response.json();
-    }).then(function (result) {
-      if(!result.error){
-        that.setState({
-          status: result.error,
-          wholeResult: result,
+    }).then((response) => response.json())
+        .then((responseJson) => {
+          console.log(responseJson);
+          return responseJson;
+        })
+        .catch((error) => {
+          console.error(error);
         });
-        console.log(that.state.wholeResult);
-      }else{
-        console.log(result);
-      }
-    }).catch(function (error) {
-      console.log("-------- error ------- "+error);
-    });
+
   }
 
-  onSubmit = () => this.registerCall();
+  onSubmit = () => this.addCall();
 
   onReject = () => this.props.history.goBack();
 
   setName = (value) => this.setState({name: value});
-  setKey = (value) => this.setState({key: value});
+  setKey = (value) => this.setState( {key: value});
   setAvailableKeys = (value) => this.setState({availableKeys: value});
   setDuration = (value) => this.setState({duration: value});
+
   render() {
     return (
-          <SoftwareDetailsComponent
-              onSubmit={this.onSubmit}
-              onReject={this.onReject}
-              setName={this.setName}
-              setKey={this.setKey}
-              setAvailableKeys={this.setAvailableKeys}
-              setDuration={this.setDuration}
-          />
+      <SoftwareDetailsComponent
+          onSubmit={this.onSubmit}
+          onReject={this.onReject}
+          setName={this.setName}
+          setKey={this.setKey}
+          setAvailableKeys={this.setAvailableKeys}
+          setDuration={this.setDuration}
+      />
     );
   }
 }
