add013981d3f85e901b8f17e9b981bfcb427a278
Kamil Rusin <lotrusek@gmail.com>
2020-01-06 20:53:08 +0100

    Pierwsza wersja serwisu dla hardwareDictionary

--- backend/src/main/java/org/polsl/backend/service/HardwareDictionaryService.java
+++ backend/src/main/java/org/polsl/backend/service/HardwareDictionaryService.java
@@ -1,15 +1,32 @@
 package org.polsl.backend.service;
 
-import org.polsl.backend.repository.HardwareRepository;
+import org.polsl.backend.dto.hardwaredictionary.HardwareDictionaryListOutputDTO;
+import org.polsl.backend.entity.HardwareDictionary;
+import org.polsl.backend.repository.HardwareDictionaryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class HardwareDictionaryService {
-  private HardwareRepository hardwareRepository;
+  private HardwareDictionaryRepository hardwareDictionaryRepository;
 
   @Autowired
-  public HardwareDictionaryService(HardwareRepository hardwareRepository) {
-    this.hardwareRepository = hardwareRepository;
+  public HardwareDictionaryService(HardwareDictionaryRepository hardwareDictionaryRepository) {
+    this.hardwareDictionaryRepository = hardwareDictionaryRepository;
+  }
+
+  public List<HardwareDictionaryListOutputDTO> getAllHardwareDictionaries() {
+    Iterable<HardwareDictionary> hardwareDictionaries = hardwareDictionaryRepository.findAll();
+    List<HardwareDictionaryListOutputDTO> dtos = new ArrayList<>();
+    for(HardwareDictionary hardwareDictionary : hardwareDictionaries){
+      HardwareDictionaryListOutputDTO dto = new HardwareDictionaryListOutputDTO();
+      dto.setId(hardwareDictionary.getId());
+      dto.setValue(hardwareDictionary.getValue());
+      dtos.add(dto);
+    }
+    return  dtos;
   }
 }
