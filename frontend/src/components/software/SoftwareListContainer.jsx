14c30eb2ae864831f602a578519974b01c21711d
Kamil Błoński <33577175+kamil25963@users.noreply.github.com>
2020-01-07 18:50:06 +0100

    Utworzenie formularza software'u
    * Dodanie przyciskow akcji dla pojedy nczych rekordów, edytuj, usuń.
    
    * Dodanie przycisku 'dodaj oprogramowanie'.
    
    * Dodanie przyciskow zapisz/wroc w formularzu.
    
    * Przycisk zapisz/usuń powodują powrót do listy oprogramowania.
    
    * Utworzenie podstawowej wersji formularza.
    
    * Formularz dodaje dane do bazy - bez walidacji, bez niczego, po prostu dodaje.
    
    * Drobne zmiany w kodzie.
    
    * Zmiany w kodzie po merge, użycie przyciskó powodowało błąd.
    
    * Różny komunikat w nagłówku w zależności od wyboru dodaj/edytuj.
    
    * commit gdybym za chwile zepsuł
    
    * Przy kliknięciu edytuj wysyłane jest zapytanie do serwera, które pobiera dane i wpisuje do formularza (formularz będzie zmieniany, sposób przekazywania i zapisywania w textinput jest ok).
    
    * Zamiana 4 metod setState na 1.
    
    * Teraz działa, błąd przy rozwiązywaniu konfliktu.
    
    * Zmiana wyglądu.
    
    * Walidacja, czy pola nie są puste (przycisk zapisz jest wyłączony jesli pola nie są uzupełnione)
    
    * Przy dodawaniu zapisuje sie duration takie, z któego moge obliczyć ilość msc pozostałych do końca licencji.
    
    * Zmiana czasu trwania licencji w tabeli software na miesiące.
    
    * Zmiana czasu trwania licencji w formularzu edycji na miesiące.
    
    * Włączenie walidacji i małe ulepszenie kodu.
    
    * Uniwersalna metoda do zapisu przy dodawaniu i edycji.
    
    * Usuwanie.
    
    * Sprawdzenie czy odpowiednie pola forumlarza to liczby, zablokowanie przycisku zapisz i wyświetlenie stosownego komunikatu.
    
    * Walidacja dla dodawania działa ok.
    
    * Walidacja ukończona.
    
    * Usunięcie consolelog
    
    * Pozbycie się błędów z konsoli.
    
    * Odświeżenie strony po usunięciu.
    
    * Drobna zmiana w wyświetlaniu kominikatów błędu.
    
    * backend, do usunięcia
    
    * naprawa po złym rozwiązaniu konfilktów
    
    * Zmiany po codereview

--- frontend/src/components/software/SoftwareListContainer.jsx
+++ frontend/src/components/software/SoftwareListContainer.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import SoftwareListComponent from './SoftwareListComponent';
-import request from '../../APIClient';
+import request from "../../APIClient";
+import moment from "moment";
 
 class SoftwareListContainer extends Component {
   constructor(props) {
@@ -13,7 +14,6 @@ class SoftwareListContainer extends Component {
       filters: {},
     };
   }
-
   componentDidMount() {
     this.fetchData();
   }
@@ -23,20 +23,29 @@ class SoftwareListContainer extends Component {
       loading: true,
       error: false,
     });
-    request('/api/software', options)
-      .then((response) => response.json())
-      .then((response) => {
-        this.setState({
-          loading: false,
-          ...response,
-        });
-      })
-      .catch(() => {
-        this.setState({
-          loading: false,
-          error: true,
-        });
-      })
+    request('/api/software')
+        .then((response) => response.json())
+        .then((response) => {
+          for(let i = 0; i < response.items.length; i++)
+          {
+            let duration = response.items[i].duration;
+            let months = moment(duration).month() +  12 * (moment(duration).year() - moment(0).year());
+            if(months <= 0)
+              response.items[i].duration = "Licencja utraciła ważność";
+            else
+              response.items[i].duration = months;
+          }
+          this.setState({
+            loading: false,
+            ...response,
+          });
+        })
+        .catch(() => {
+          this.setState({
+            loading: false,
+            error: true,
+          });
+        })
   };
 
   handleFilterChange = (fieldName, text) => {
@@ -50,6 +59,23 @@ class SoftwareListContainer extends Component {
     this.fetchData({
       filters: newFilters,
     });
+  };
+
+  deleteCall = (id) => {
+    request(`http://localhost:8080/api/software/${id}`,{
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }
+    }).then((response) => response.json())
+        .then((responseJson) => {
+          console.log(responseJson);
+          window.location.reload();
+        })
+        .catch((error) => {
+          console.error(error);
+        });
   };
 
   render() {
@@ -74,7 +100,7 @@ class SoftwareListContainer extends Component {
       },
       {
         name: 'duration',
-        label: 'Ważna do',
+        label: 'Ważna przez (msc)',
       },
     ];
 
@@ -88,9 +114,7 @@ class SoftwareListContainer extends Component {
       },
       {
         label: 'Usuń',
-        onClick: (itemData) => {
-          // TODO: usuwanie software'u
-        },
+        onClick: (itemData) => {this.deleteCall(itemData.id)},
       },
       // TODO: akcje wyświetlania historii powiązań
     ];
