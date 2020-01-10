e4a74763039f6f243fb149270a58bec3706383b1
Bartłomiej Szlachta <bartlomiej.szlachta@softiq.pl>
2020-01-10 10:14:10 +0100

    Utworzenie szkieletu formularza

--- frontend/src/components/affiliations/AffiliationDetailsComponent.jsx
+++ frontend/src/components/affiliations/AffiliationDetailsComponent.jsx
@@ -1,24 +1,77 @@
 import React from 'react';
-import {Button, StyleSheet, Text} from 'react-native';
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {mainColor} from '../../constValues';
 
 const AffiliationDetailsComponent = (props) => {
-  return (
-    <>
-      <Text>Tutaj znajdzie się formularz przynależności</Text>
+  const {
+    onSubmit,
+    onReject,
+    mode,
+  } = props;
+
+  const header = (
+    <View style={styles.header}>
+      <Text style={styles.headerText}>
+        {mode === 'create' && 'Dodawanie osoby / miejsca'}
+        {mode === 'edit' && 'Edycja osoby / miejsca'}
+      </Text>
+    </View>
+  );
+
+  const main = (
+    <View style={styles.main}>
+      Główna część formularza
+    </View>
+  );
+
+  const footer = (
+    <View style={styles.footer}>
       <Button
         title="Zapisz"
-        onPress={props.onSubmit}
+        onPress={onSubmit}
+        color={mainColor}
       />
       <Button
         title="Wróć"
-        onPress={props.onReject}
+        onPress={onReject}
+        color={mainColor}
       />
-    </>
+    </View>
+  );
+
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        {header}
+        {main}
+        {footer}
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
 
+  },
+  header: {
+    flex: 1,
+  },
+  headerText: {
+    fontSize: 20,
+  },
+  main: {
+    flex: 1,
+  },
+  footer: {
+    flex: 1,
+  },
 });
 
 export default AffiliationDetailsComponent;
--- frontend/src/components/affiliations/AffiliationDetailsContainer.jsx
+++ frontend/src/components/affiliations/AffiliationDetailsContainer.jsx
@@ -1,19 +1,55 @@
 import React, {Component} from 'react';
 import AffiliationDetailsComponent from './AffiliationDetailsComponent';
+import request from '../../APIClient';
 
 class AffiliationDetailsContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {},
+      isLoading: true,
+      error: false,
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.mode === 'edit') {
+      this.loadInitialData();
+    }
+  }
+
+  loadInitialData = () => {
+    request(`/api/affiliations/${this.props.id}`)
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({
+          data: response,
+          isLoading: false,
+        });
+      })
+      .catch((response) => {
+        this.setState({
+          error: true,
+        });
+      });
+  };
 
   onSubmit = () => this.props.goBack();
 
   onReject = () => this.props.goBack();
 
   render() {
-    console.log(`Przykład uzyskania id elementu: ${this.props.id}`);
-    console.log(`Przykład uzyskania trybu formularza: ${this.props.mode}`);
+    const {mode} = this.props;
+    const {data, isLoading, error} = this.state;
+
     return (
       <AffiliationDetailsComponent
         onSubmit={this.onSubmit}
         onReject={this.onReject}
+        mode={mode}
+        data={data}
+        isLoading={isLoading}
+        error={error}
       />
     );
   }
