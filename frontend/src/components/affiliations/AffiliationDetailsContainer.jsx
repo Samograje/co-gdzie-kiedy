9e5bba120241cef2f2e13d62acdc4d94614ee57c
Bartłomiej Szlachta <bartlomiej.szlachta@softiq.pl>
2020-01-11 00:02:17 +0100

    Utworzenie formularza

--- frontend/src/components/affiliations/AffiliationDetailsContainer.jsx
+++ frontend/src/components/affiliations/AffiliationDetailsContainer.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Dimensions} from 'react-native';
 import AffiliationDetailsComponent from './AffiliationDetailsComponent';
 import request from '../../APIClient';
 
@@ -6,9 +7,16 @@ class AffiliationDetailsContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: {},
-      isLoading: true,
+      data: {
+        firstName: '',
+        lastName: '',
+        location: '',
+      },
       error: false,
+      errors: {},
+      isLoading: false,
+      isSubmitting: false,
+      isWide: Dimensions.get('window').width > 650,
     };
   }
 
@@ -19,6 +27,9 @@ class AffiliationDetailsContainer extends Component {
   }
 
   loadInitialData = () => {
+    this.setState({
+      isLoading: true,
+    });
     request(`/api/affiliations/${this.props.id}`)
       .then((response) => response.json())
       .then((response) => {
@@ -27,29 +38,81 @@ class AffiliationDetailsContainer extends Component {
           isLoading: false,
         });
       })
-      .catch((response) => {
+      .catch(() => {
         this.setState({
+          isLoading: false,
           error: true,
         });
       });
   };
 
-  onSubmit = () => this.props.goBack();
+  onChange = (fieldName, value) => {
+    const newErrors = {...this.state.errors};
+    if (!value) {
+      newErrors[fieldName] = 'To pole jest wymagane';
+    } else {
+      newErrors[fieldName] = null;
+    }
+    this.setState((prevState) => ({
+      data: {
+        ...prevState.data,
+        [fieldName]: value,
+      },
+      errors: newErrors,
+    }));
+  };
+
+  onSubmit = () => {
+    const errors = {};
+    Object.keys(this.state.data).forEach((key) => {
+      const value = this.state.data[key];
+      if (!value) {
+        errors[key] = 'To pole jest wymagane';
+      }
+    });
+    const noErrors = Object.keys(errors).length === 0;
+    if (!noErrors) {
+      this.setState({
+        errors,
+      });
+    } else {
+      this.setState({
+        isSubmitting: true,
+      });
+      console.log(this.state.data);
+    }
+  };
 
   onReject = () => this.props.goBack();
 
+  onLayout = () => {
+    this.setState({
+      isWide: Dimensions.get('window').width > 650,
+    });
+  };
+
   render() {
     const {mode} = this.props;
-    const {data, isLoading, error} = this.state;
+    const {
+      error,
+      errors,
+      isLoading,
+      isSubmitting,
+      isWide,
+    } = this.state;
 
     return (
       <AffiliationDetailsComponent
+        mode={mode}
+        error={error}
+        errors={errors}
+        isLoading={isLoading}
+        isSubmitting={isSubmitting}
+        isWide={isWide}
         onSubmit={this.onSubmit}
         onReject={this.onReject}
-        mode={mode}
-        data={data}
-        isLoading={isLoading}
-        error={error}
+        onChange={this.onChange}
+        onLayout={this.onLayout}
       />
     );
   }
